91720cf7ae114b8c30a009852086fef072d0bd8f
Valdaserides <max.valdaseridesolofsson.te16c@gmail.com>
2018-04-13 15:39:51 +0200

    -

--- templates/navigation.php
+++ templates/navigation.php
@@ -4,6 +4,8 @@
 
 <link rel="stylesheet" href="../css/navigation.css" />
 <link href="https://fonts.googleapis.com/css?family=Alfa Slab One" rel="stylesheet">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <style>
 
@@ -13,7 +15,7 @@
 <body>
 
 <nav>
-	<a class="burger-nav"></a>
+
 	<ul>
 	  <li><a class="active" href="/dawei/hem/hemIndex.php">HEM</a></li>
 	  <li><a href="/dawei/meny/menuIndex.php">MENY</a></li>
@@ -21,4 +23,22 @@
 	  <li><a href="/dawei/omdomen/omdomenIndex.php">OMDÖMEN</a></li>
 	  <li><a href="/dawei/bokaBord/bokaBordIndex.php" id="bokaBord">BOKA BORD</a></li>
 	</ul>
+
 </nav>
+	
+<nav id="respo-menu">
+	
+	<label for="toggle"> &#9776; </label>
+	<input type="checkbox" id="toggle" />
+		
+	<ul class="respo-li">
+		<li><a class="active" href="/dawei/hem/hemIndex.php">HEM</a></li>
+		<li><a href="/dawei/meny/menuIndex.php">MENY</a></li>
+		<li><a href="/dawei/about/aboutIndex.php">OM OSS</a></li>
+		<li><a href="/dawei/omdomen/omdomenIndex.php">OMDÖMEN</a></li>
+		<li><a href="/dawei/bokaBord/bokaBordIndex.php" id="bokaBord">BOKA BORD</a></li>
+	</ul>
+
+</nav>
+	
+
