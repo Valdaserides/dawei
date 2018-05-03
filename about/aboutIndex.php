973e62cff78c0299c7d21ed1d735846db66bed87
Valdaserides <max.valdaseridesolofsson.te16c@gmail.com>
2018-05-03 12:09:19 +0200

    -

--- about/aboutIndex.php
+++ about/aboutIndex.php
@@ -5,8 +5,39 @@ include("../templates/navigation.php");
 ?>
 <link rel="stylesheet" href="../css/aboutCss.css">
 
-om oss
+<main>
 
+	<h1> Dawei thairestaurang </h1>
+
+	<div class="main-content">
+		
+		<section class="no-border">
+		
+			<h3> Öppetider: </h3> <br>
+			<p> Måndag - fredag: 10:00 - 19:00 <br> <br> </p>
+			<p> Lördag: 10:00 - 19:00 <br> <br> </p>
+			<p> Söndag: 12:00 - 18:00 </p>
+			
+		</section>
+		
+		<section>
+		
+			<h3> Adress: </h3> <br>
+			<p> Kronhusgatan 9, 411 05 Göteborg </p> <br>
+			<p> 031 - 85 85 85 </p>	<br>
+			<p> support@dawei.se </p>
+		
+		</section>
+	
+	</div>
+
+</main>
 
 </body>
 </html>
+
+<?php
+
+include("../templates/footer.php");
+
+?>
